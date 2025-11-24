fb41f6bbff746e88852d7de46a7582f8b7c677ef
Mats von Weissenberg <mats.weissenberg@afry.com>
2025-11-24 16:04:39 +0200

    Exercise 1.13

--- part_1/src/App.jsx
+++ part_1/src/App.jsx
@@ -16,13 +16,13 @@ const Statistics = (props) => {
   if (good + neutral + bad === 0) {
     return <div>No feedback given!</div>
   }
-  return(
-      <table>
-      <StatisticLine text="good" value ={good} />
-      <StatisticLine text="neutral" value ={neutral} />
-      <StatisticLine text="bad" value ={bad} />
-      <StatisticLine text="all" value ={good + neutral + bad} />
-      <StatisticLine text="average" value ={(good - bad) / (good + neutral + bad)} />
+  return (
+    <table>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={good + neutral + bad} />
+      <StatisticLine text="average" value={(good - bad) / (good + neutral + bad)} />
     </table>
   )
 }
@@ -39,21 +39,39 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ]
-   
+
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+  const [currentAnecdote, setCurrentAnecdote] = useState(0)
   const [selected, setSelected] = useState(0)
-  
+
   function getRandomNumber(n) {
     return Math.floor(Math.random() * (n + 1));
   }
 
+  function nextAnecdote() {
+    const randomNumber = getRandomNumber(anecdotes.length - 1);
+    setCurrentAnecdote(randomNumber);
+    setSelected(randomNumber);
+  } 
+
+  function vote() {
+    const copy = [...votes];
+    copy[selected] += 1;
+    setVotes(copy);
+  }
+
   return (
     <div>
-    <div>
-      {anecdotes[selected]}
-    </div>
-      <button onClick={()=>setSelected(getRandomNumber(anecdotes.length -1))}>
-      next anecdote
-    </button>
+      <div>
+        {anecdotes[selected]}
+        <div>has {votes[selected]} votes</div>
+      </div>
+      <button onClick={nextAnecdote}>
+        next anecdote
+      </button>
+      <button onClick={vote}>
+        vote
+      </button>
     </div>
   )
 }
