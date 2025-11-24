dc76198b47034eb55a4f8f511fdb6457eb204d18
Mats von Weissenberg <mats.weissenberg@afry.com>
2025-11-24 15:06:16 +0200

    Exercise 1.6

--- part_1/src/App.jsx
+++ part_1/src/App.jsx
@@ -1,71 +1,29 @@
+import { useState } from 'react'
 
-const Header = (props) => {
-  console.log(props)
-  return (
-    <h1>
-      {props.course}
-    </h1>
-  )
-}
+const Display = (props) => <div>{props.caption}: {props.value}</div>
 
-const Part = (props) => {
-  console.log(props);
-  const { part } = props;
-  return (
-      <p>
-        {part.name} {part.exercises}
-      </p>
-  )
-}
-
-const Content = (props) => {
-  console.log(props);
-  const { course } = props;
-  return (
-    <div>
-      {course.parts.map((part, index) => (
-        <p key={`part_${index}`}>
-          {part.name} {part.exercises}
-        </p>
-      ))}
-    </div>
-  )
-}
-
-const Total = (props) => {
-  console.log(props)
-  const { course } = props;
-  return (
-    <p>
-      Number of exercises {course.parts.reduce((accumulator, part) => accumulator + part.exercises, 0)}
-    </p>
-  )
-}
+const Button = (props) => (
+  <button onClick={props.onClick}>
+    {props.text}
+  </button>
+)
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content course={course} />
-      <Total course={course} />
+      <h1>give feedback</h1>
+      <Button onClick={() => setGood(good+1)} text="good" />
+      <Button onClick={() => setNeutral(neutral+1)} text="neutral" />
+      <Button onClick={() => setBad(bad+1)} text="bad" />
+      <h1>statistics</h1>
+      <Display caption="good" value={good} />
+      <Display caption="neutral" value={neutral} />
+      <Display caption="bad" value={bad} /> 
     </div>
   )
 }
