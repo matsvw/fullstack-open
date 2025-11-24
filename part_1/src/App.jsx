59c2aa34b0bdf22106e390486f3a75c7c0c012d7
Mats von Weissenberg <mats.weissenberg@afry.com>
2025-11-24 15:20:32 +0200

    Exercise 1.12

--- part_1/src/App.jsx
+++ part_1/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-const Display = (props) => <div>{props.caption}: {props.value}</div>
+const Display = (props) => <tr><td>{props.caption}</td><td>{props.value}</td></tr>
 
 const Button = (props) => (
   <button onClick={props.onClick}>
@@ -17,34 +17,45 @@ const Statistics = (props) => {
     return <div>No feedback given!</div>
   }
   return(
-    <div>
+      <table>
       <StatisticLine text="good" value ={good} />
       <StatisticLine text="neutral" value ={neutral} />
       <StatisticLine text="bad" value ={bad} />
       <StatisticLine text="all" value ={good + neutral + bad} />
       <StatisticLine text="average" value ={(good - bad) / (good + neutral + bad)} />
-    </div>
+    </table>
   )
 }
 
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  
+  function getRandomNumber(n) {
+    return Math.floor(Math.random() * (n + 1));
+  }
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <Button onClick={() => setGood(good+1)} text="good" />
-      <Button onClick={() => setNeutral(neutral+1)} text="neutral" />
-      <Button onClick={() => setBad(bad+1)} text="bad" />
-      <h1>statistics</h1>
-      <Statistics good={good} neutral={neutral} bad={bad} />
+    <div>
+      {anecdotes[selected]}
+    </div>
+      <button onClick={()=>setSelected(getRandomNumber(anecdotes.length -1))}>
+      next anecdote
+    </button>
     </div>
   )
 }
-
 
 export default App
