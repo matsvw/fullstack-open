fa0da1998f99c79980678cb29cd8e77053c4bc81
Mats von Weissenberg <mats.weissenberg@afry.com>
2025-11-24 11:31:03 +0200

    exercise 1.1

---
+++ part_1/src/App.jsx
@@ -0,0 +1,52 @@
+
+const Header = (props) => {
+  console.log(props)
+  return (
+    <h1>
+      {props.course}
+    </h1>
+  )
+}
+
+const Content = (props) => {
+  console.log(props);
+  const { parts, exercises } = props;
+  return (
+    <div>
+      {parts.map((part, index) => (
+        <p key={`part_${index}`}>
+          {part} {exercises[index]}
+        </p>
+      ))}
+    </div>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+  return (
+    <p>
+      Number of exercises {props.total}
+    </p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
+      <Total total={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+export default App
