9cc529794ce7609df1f854ef9043aa244d7d956b
Mats von Weissenberg <mats.weissenberg@afry.com>
2025-11-25 11:53:55 +0200

    Exercise 2.6

--- part_2/src/App.jsx
+++ part_2/src/App.jsx
@@ -1,57 +1,40 @@
-//import { useState } from 'react'
-
-// Course information already moved to Course component
-import Course from './components/Course';
+import { useState } from 'react'
 
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event)
+    setPersons(persons.concat({ name: newName }))
+    setNewName('')
+  }
 
   return (
     <div>
-      {courses.map(course => <Course key={course.id} course={course} />)}
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit" onClick={addPerson}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person, index) =>
+        <div key={`${person.name}_${index}`}>{person.name}</div>
+      )}
+      <br />
+      <br />
+      <div>debug: {newName}</div>
     </div>
   )
 }
