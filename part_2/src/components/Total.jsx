6d7b8870c632eac318bd31918f309ea9bf75b874
Mats von Weissenberg <mats.weissenberg@afry.com>
2025-11-25 10:42:11 +0200

    Exercise 2.4

--- part_2/src/components/Total.jsx
+++ part_2/src/components/Total.jsx
@@ -1,7 +1,7 @@
 const Total = (props) => {
     const {parts} = props.course;
     const total = parts.reduce((s, p) => {
-        console.log('what is happening', s, p)
+        //console.log('what is happening', s, p)
         return s + p.exercises
     },0)
     return <p><b>total of {total} exercises</b></p>
