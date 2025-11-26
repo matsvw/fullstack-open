ba06cb0be4af1aea4a449055d1e9397ff4053812
Mats von Weissenberg <mats.weissenberg@afry.com>
2025-11-26 09:12:47 +0200

    Exercise 3.8

--- part_3/index.js
+++ part_3/index.js
@@ -17,7 +17,11 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(express.json())
-app.use(morgan('tiny'))
+//app.use(morgan('tiny'))
+
+morgan.token('body', (req) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
 //app.use(requestLogger)
 
 let persons = [
