a2129ebd87622731481c0f832ce0c30d2e619125
Mats von Weissenberg <mats.weissenberg@afry.com>
2025-11-26 16:08:14 +0200

    Exercise 3.20-3.21

--- part_3/models/person.js
+++ part_3/models/person.js
@@ -19,7 +19,16 @@ const personSchema = new mongoose.Schema({
         minLength: 3,
         required: true
     },
-    number: String,
+    number: {
+        type: String,
+        validate: {
+            validator: function (v) {
+                return /(?:\d{2}-\d{6,}|\d{3}-\d{5,})/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+        required: true
+    }
 })
 
 
