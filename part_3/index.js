97d8ccd158df969821e5095e4ba4500fd141aca2
Mats von Weissenberg <mats.weissenberg@afry.com>
2025-11-26 09:06:16 +0200

    Exercise 3.7

--- part_3/index.js
+++ part_3/index.js
@@ -1,8 +1,24 @@
 
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
 app.use(express.json())
+app.use(morgan('tiny'))
+//app.use(requestLogger)
 
 let persons = [
     { 
@@ -92,6 +108,8 @@ app.delete('/api/persons/:id', (request, response) => {
 
   response.status(204).end()
 })
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
