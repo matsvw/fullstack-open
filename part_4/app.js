157ab7fdb7baefd82d8f20708f676a265f019ea5
Mats von Weissenberg <mats.weissenberg@afry.com>
2025-11-30 18:56:11 +0200

    Preparations for token validation

--- part_4/app.js
+++ part_4/app.js
@@ -4,6 +4,7 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 
+const loginRouter = require('./controllers/login')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 
@@ -26,6 +27,7 @@ app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 
