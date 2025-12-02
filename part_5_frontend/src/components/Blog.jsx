cfc613990684df7006bc529bc37ba524e5398eec
Mats von Weissenberg <mats.weissenberg@afry.com>
2025-12-02 10:36:49 +0200

    Exercise 5.7

--- part_5_frontend/src/components/Blog.jsx
+++ part_5_frontend/src/components/Blog.jsx
@@ -1,7 +1,30 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  return (
+    <div className="blogStyle">
+      <b>{blog.title}</b>
+      <input type="button" value={showDetails ? "hide" : "view"} onClick={() => setShowDetails(!showDetails)} style={{ float: "right" }} />
+      {showDetails && 
+        <BlogDetails blog={blog} />
+      }
+    </div>
+  )
+}
+
+const BlogDetails = ({ blog }) => {
+  return (
+    <div>
+      <div>{blog.author}</div>
+      <div>{blog.url}</div>
+      <div>
+        likes {blog.likes}
+        <button style={{ marginLeft: "10px" }}>like</button>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
