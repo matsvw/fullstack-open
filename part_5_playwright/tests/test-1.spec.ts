bb1c2a09ad465e722609e77ca027603f9de85f22
Mats von Weissenberg <mats.weissenberg@afry.com>
2025-12-03 18:13:45 +0200

    Exercise 6.1

--- part_5_playwright/tests/test-1.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
-await page.goto('chrome-error://chromewebdata/');await page.goto('chrome-error://chromewebdata/');
-});
