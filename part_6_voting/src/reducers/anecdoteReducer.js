8884d3f30b83fde9bcb98a01b42a2d43e686b6ca
Mats von Weissenberg <mats.weissenberg@afry.com>
2025-12-03 18:49:24 +0200

    Exercise 6.3

---
+++ part_6_voting/src/reducers/anecdoteReducer.js
@@ -0,0 +1,48 @@
+const anecdotesAtStart = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+const getId = () => (100000 * Math.random()).toFixed(0)
+
+const asObject = anecdote => {
+  return {
+    content: anecdote,
+    id: getId(),
+    votes: 0
+  }
+}
+
+const initialState = anecdotesAtStart.map(asObject)
+
+export const voteForAnecdote = id => {
+  return {
+    type: 'VOTE',
+    data: { id }
+  }
+}
+
+const reducer = (state = initialState, action) => {
+  console.log('state now: ', state)
+  console.log('action', action)
+  const id = action.data?.id
+
+  switch (action.type) {
+    case 'VOTE':
+      return state.map(anecdote =>
+        anecdote.id === id
+          ? { ...anecdote, votes: anecdote.votes + 1 }
+          : anecdote
+      )
+    default:
+      console.log(`Unsupported action '${action.type}'. Returning default state`)
+      return state
+  }
+
+}
+
+export default reducer
