69ea5150c009a7bb520bdd65d93990a452c86571
Mats von Weissenberg <mats.weissenberg@afry.com>
2025-12-11 15:38:36 +0200

    Preparing MongoDB connection

--- part_8_library/index.js
+++ part_8_library/index.js
@@ -3,89 +3,24 @@ const { GraphQLError } = require('graphql')
 const { startStandaloneServer } = require('@apollo/server/standalone')
 const { v1: uuid } = require('uuid')
 
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821
-  },
-  {
-    name: 'Joshua Kerievsky', // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  {
-    name: 'Sandi Metz', // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-]
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
+const Book = require('./models/book')
+const Author = require('./models/author')
 
-/*
- * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
-*/
+require('dotenv').config()
 
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
-  },
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
-  },
-  {
-    title: 'Demons',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
-  },
-]
+const MONGODB_URI = process.env.MONGODB_URI
+
+console.log('connecting to', MONGODB_URI)
+
+mongoose.connect(MONGODB_URI)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
 
 const typeDefs = /* GraphQL */`
   type Book {
@@ -126,32 +61,35 @@ const typeDefs = /* GraphQL */`
 
 const resolvers = {
   Query: {
-    bookCount: () => books.length,
-    authorCount: () => authors.length,
-    allBooks: (root, args) => {
-      let filteredBooks = books
+    bookCount: () => async () => Book.collection.countDocuments(),
+    authorCount: () => async () => Author.collection.countDocuments(),
+    allBooks: async (root, args) => {
+      const filter = {}
+
+      //TODO - currently this is not case insensitive
       if (args.author) {
-        const auth = args.author.toLowerCase()
-        filteredBooks = filteredBooks.filter(b => b.author.toLowerCase().includes(auth)) // partial, case insensitive, match ok for author
+        filter.author = args.author
       }
       if (args.genre) {
-        const genre = args.genre.toLowerCase()
-        filteredBooks = filteredBooks.filter(b => b.genres.map(g => g.toLowerCase()).includes(genre)) // genre name must match, but is not case sensitive
+        filter.genres = args.genre //this should match any
       }
-
+      const filteredBooks = Book.find(filter)
       return filteredBooks
     },
 
-    allAuthors: (root, args, context, info) => {
+    allAuthors: async (root, args, context, info) => {
 
       const selections = info.fieldNodes?.[0]?.selectionSet?.selections ?? []
       const requestedBookCount = selections.some(sel => sel.kind === 'Field' && sel.name.value === 'bookCount')
 
+      //TODO - could this be done with a single roundtrip?
+      const authors = Author.find({})
       if (!requestedBookCount) {
         return authors
       }
       console.log('Calculating book count for each author')
       const authorBookCount = authors.map(a => {
+        const books = Book.find({})
         const bookCount = books.filter(b => b.author === a.name).length
         return { ...a, bookCount }
       })
@@ -159,7 +97,7 @@ const resolvers = {
     },
   },
   Mutation: {
-    addBook: (root, args) => {
+    addBook: async (root, args) => {
 
       // Apparently the default check treats an empty string as a valid value
       if (!args.title || !args.author) {
@@ -171,32 +109,26 @@ const resolvers = {
         })
       }
 
-      if (books.find(p => p.title.toLowerCase() === args.title.toLowerCase())) { //keeping this as it makes sense.
-        throw new GraphQLError('Title must be unique', {
-          extensions: {
-            code: 'BAD_USER_INPUT',
-            invalidArgs: args.title
-          }
-        })
-      }
+      // Unique check for title removed as MongoDB should handle that
 
-      let author = authors.find(a => a.name.toLowerCase() === args.author.toLowerCase())
+      //TODO - this is now not case insensitive
+      const author = await Author.findOne({ name: args.author })
       if (!author) {
-        author = { name: args.author, id: uuid() } //birthyear not known
-        authors = authors.concat(author)
+        const newAuthor = new Author({ name: args.author }) //birthyear not known
+        newAuthor.save()
       }
 
-      const book = { ...args, author: author.name, id: uuid() } // update author name to ensure case matches
-      books = books.concat(book)
-      return book
+      const newBook = new Book({ ...args })
+      newBook.save()
+      return newBook
     },
-    editAuthor: (root, args) => {
-      const author = authors.find(a => args.name.toLowerCase() === a.name.toLowerCase())
+    editAuthor: async (root, args) => {
+      //TODO - this is now not case insensitive
+      const author = await Author.findOne({ name: args.name })
       if (author) {
         author.born = args.setBornTo
-        authors = authors.map(a => a.id === author.id ? author : a)
+        author.save()
       }
-
       return author
     },
   }
