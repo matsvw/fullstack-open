a7f326499a55a334c23f865374db02a3dbbdcac4
Mats von Weissenberg <mats.weissenberg@afry.com>
2025-12-15 19:55:18 +0200

    Exercise 9.22

---
+++ part_9/part_9_patientor_ui/src/components/PatientDetailsPage/index.tsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box, Typography } from "@mui/material";
+import FemaleIcon from "@mui/icons-material/Female";
+import MaleIcon from "@mui/icons-material/Male";
+import TransgenderIcon from "@mui/icons-material/Transgender";
+
+import { Patient, Gender } from "../../types";
+
+import patientService from "../../services/patients";
+
+const PatientDetailsPage = () => {
+  const [patient, setPatient] = useState<Patient>();
+  const { id: patientId } = useParams();
+
+  useEffect(() => {
+    const getPatient = async (id: string) => {
+      const pt = await patientService.findById(id);
+      setPatient(pt);
+    };
+
+    if (patientId) {
+      getPatient(patientId);
+    }
+  }, [patientId]);
+
+  if (!patient) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div className="App">
+      <Box sx={{ mt: "2rem" }}>
+        <Typography variant="h6">
+          {patient.name}
+          <Box sx={{ ml: "1rem", display: "inline" }}>
+            {patient.gender === Gender.Male && <MaleIcon />}
+            {patient.gender === Gender.Female && <FemaleIcon />}
+            {patient.gender === Gender.Other && <TransgenderIcon />}
+          </Box>
+        </Typography>
+        <Typography variant="body1">ssn: {patient.ssn ?? "unknown"}</Typography>
+        <Typography variant="body1">
+          dob: {patient.dateOfBirth ?? "unknown"}
+        </Typography>
+        <Typography variant="body1">
+          occupation: {patient.occupation}
+        </Typography>
+        {patient.entries.map((_e, i) => (
+          <Typography key={`entry_${i}`} variant="body1">
+            entry todo
+          </Typography>
+        ))}
+      </Box>
+    </div>
+  );
+};
+
+export default PatientDetailsPage;
